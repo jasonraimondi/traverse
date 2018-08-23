85d5d25c6ed9d7fcfe2b574ccf54cddcf36ece57
Jason Raimondi <jason@raimondi.us>
2018-08-23 12:20:08 -0700

    Remove jest config from package.json, move to jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  "setupFiles": [
+    "<rootDir>/test/setupTests.ts"
+  ]
+};
