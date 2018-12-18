6d4601f325f00448f85714545915282bafc7920f
Jason Raimondi <jason@raimondi.us>
2018-12-18 11:23:47 -0800

    Fix ts-jest path mappings.

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,25 @@
 module.exports = {
-  "collectCoverage": true,
-  "collectCoverageFrom": ["src/{app,infrastructure,models}/**/*.{js,jsx,ts,tsx}"],
-  "coverageReporters": ["lcov"],
-  "transform": {
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-  "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
-  "setupFiles": [
-    "<rootDir>/test/setupTests.ts"
-  ],
-  "moduleNameMapper": {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/fileMock.ts",
-    "\\.(css|pcss)$": "<rootDir>/test/styleMock.ts"
-  }
+    preset: "ts-jest",
+    collectCoverage: true,
+    collectCoverageFrom: ["src/{app,infrastructure,models}/**/*.{js,jsx,ts,tsx}"],
+    coverageReporters: ["lcov"],
+    transform: {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    moduleFileExtensions: [
+        "ts",
+        "tsx",
+        "js",
+        "jsx",
+        "json",
+        "node"
+    ],
+    setupFiles: [
+        "<rootDir>/test/setupTests.ts"
+    ],
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/fileMock.ts",
+        "\\.(css|pcss)$": "<rootDir>/test/styleMock.ts",
+        "@/(.*)$": "<rootDir>/src/$1"
+    }
 };
