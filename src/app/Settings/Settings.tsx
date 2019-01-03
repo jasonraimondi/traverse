8514b69276f649722a07a76aec28c979c00b46e4
Jason Raimondi <jason@raimondi.us>
2019-01-03 11:28:16 -0800

    Wip add/edit github access token settings page
    This will allow users to set a github access token so they wont be throttled.

--- src/app/Settings/Settings.tsx
+++ src/app/Settings/Settings.tsx
@@ -3,31 +3,82 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 
-import { SetGithubAccessTokenAction } from '@/infrastructure/redux/actions/SetGithubAccessToken.action';
+import {
+  SetGithubAccessTokenAction,
+  SetGithubAccessTokenActionType,
+} from '@/infrastructure/redux/actions/SetGithubAccessToken.action';
 
 interface Props {
   githubAccessToken: string;
+  SetGithubAccessTokenAction: SetGithubAccessTokenActionType;
 }
 
-class Settings extends React.Component<Props> {
-    render() {
-        return <div>Github AccessToken: {this.props.githubAccessToken}</div>;
-    }
+interface State {
+  isEditMode: boolean;
+  githubAccessToken: string;
+}
+
+class Settings extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      isEditMode: false,
+      githubAccessToken: props.githubAccessToken,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ githubAccessToken: event.target.value });
+  }
+
+  handleSubmit(event) {
+    this.props.SetGithubAccessTokenAction(this.state.githubAccessToken);
+    event.preventDefault();
+  }
+
+  render() {
+    const showEditForm = this.state.isEditMode || !this.props.githubAccessToken;
+
+    const form = <form onSubmit={this.handleSubmit}>
+      <label>
+        Github Access Token:
+        <input type='text'
+               value={this.state.githubAccessToken}
+               onChange={this.handleChange}
+        />
+      </label>
+      <input type='submit' value='Submit'/>
+    </form>;
+
+    const block = <div>
+      Github Access Token: {this.props.githubAccessToken}
+    </div>;
+
+    return <div>
+      <button onClick={() => this.setState({ isEditMode: !this.state.isEditMode })}>
+        Toggle Edit Mode
+      </button>
+      {showEditForm ? form : block}
+    </div>;
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        githubAccessToken: state.githubAccessToken,
-    };
+  return {
+    githubAccessToken: state.githubAccessToken,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators(
-      {
-          SetGithubAccessTokenAction,
-      },
-      dispatch,
-    );
+  return bindActionCreators(
+    {
+      SetGithubAccessTokenAction,
+    },
+    dispatch,
+  );
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Settings);
