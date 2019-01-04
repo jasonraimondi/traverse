ac94a3d50d688a25fbce2f21f42da905b1e8e503
Jason Raimondi <jason@raimondi.us>
2019-01-04 11:04:26 -0800

    Adding some donation links.

--- src/app/About/About.tsx
+++ src/app/About/About.tsx
@@ -9,12 +9,25 @@ class About extends React.Component {
   render() {
     return <>
       <AboutContainer>
-        <TraverseTitle>
-          <Icon src='../resources/icon.png' alt='Traverse Icon'/>
-          Traverse
-          <br/>
-          <Version>v{remote.app.getVersion()}</Version>
-        </TraverseTitle>
+        <Main>
+          <TraverseTitle>
+            <Icon src='../resources/icon.png' alt='Traverse Icon'/>
+            Traverse
+            <br/>
+            <Version>v{remote.app.getVersion()}</Version>
+          </TraverseTitle>
+          <DonateTitle>Beer Me</DonateTitle>
+          <DonationAddress>
+            Ether
+            <br/>
+            <code>0x13EcA749be300C1951bD6E9d86bB395e2734BE20</code>
+          </DonationAddress>
+          <DonationAddress>
+            Nano
+            <br/>
+            <code>xrb_3ii5t9x8a7or17zkf6syp7ntprd77z7tx9migsghb9rx5ymiop76kyrahef8</code>
+          </DonationAddress>
+        </Main>
         <Copyright>Copyright © 2019 Jason Raimondi, Digital Canvas Design LLC</Copyright>
       </AboutContainer>
     </>;
@@ -24,6 +37,26 @@ class About extends React.Component {
 const Icon = styled.img`
     width: 175px;
     height: 175px;
+`;
+
+const DonateTitle = styled.p`
+    font-size: 0.8rem;
+    font-weight: 600;
+`;
+
+const DonationAddress = styled.p`
+    color: ${theme.colors.grey};
+    font-size: 0.6rem;
+    font-weight: 600;
+    & code {
+      color: ${theme.colors.purple};
+    }
+`;
+
+const Main = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
 `;
 
 const AboutContainer = styled.div`
@@ -36,13 +69,14 @@ const AboutContainer = styled.div`
     height: 100%;
 `;
 
-const TraverseTitle = styled.h2`
+const TraverseTitle = styled.div`
     flex: 1;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
 `;
+
 const Version = styled.span`
     font-size: 0.85rem;
     color: ${theme.colors.grey}
