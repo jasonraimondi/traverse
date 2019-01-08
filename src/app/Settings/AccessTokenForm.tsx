67a344fb974a84ae4b6af2b6b0b28b5bbf9a991b
Jason Raimondi <jason@raimondi.us>
2019-01-07 23:14:14 -0800

    Wip github access token form styling and functionality.

--- src/app/Settings/AccessTokenForm.tsx
+++ src/app/Settings/AccessTokenForm.tsx
@@ -1,22 +1,19 @@
-import { Field, Form, Formik, FormikActions } from 'formik';
+import { Form, Formik, FormikActions } from 'formik';
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import styled from 'styled-components';
 
-import { PillButton } from '@/app/elements/base';
 import { theme } from '@/infrastructure/styles/theme';
 
 interface Props {
   title: string;
   small?: string;
   formValue: string;
-
   handleSubmit(accessToken: string): void;
 }
 
 interface State {
-  isEditMode: boolean;
   formValue: string;
 }
 
@@ -25,48 +22,57 @@ interface FormValues {
 }
 
 export class AccessTokenForm extends React.Component<Props, State> {
-  readonly editIcon = require('@/assets/icons/icon-edit.svg');
-
   constructor(props: Props) {
     super(props);
     this.state = {
-      isEditMode: false,
       formValue: props.formValue || '',
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(values: FormValues, { setSubmitting }: FormikActions<FormValues>) {
-    setTimeout(() => setSubmitting(false), 500);
     this.props.handleSubmit(values.githubAccessToken);
+    setTimeout(() => setSubmitting(false), 500);
   }
 
   render() {
-    const isEditable = this.state.isEditMode;
     const small = this.props.small ? <Small>{this.props.small}</Small> : null;
 
-    return <Formik
-      initialValues={{
-        githubAccessToken: '',
-      }}
-      onSubmit={this.handleSubmit}
-      render={() => (
-        <Form>
-          <Label htmlFor='githubAccessToken'>
-            <Title>{this.props.title}</Title>
-            {small}
-          </Label>
-          <Field id='githubAccessToken'
-                 name='githubAccessToken'
-                 placeholder='github-personal-access-token'
-                 type='text'
-          />
-          <Submit type='submit'>Submit</Submit>
-        </Form>
-      )}
-    />;
+    return <>
+      <FormContainer>
+        <Formik
+          initialValues={{
+            githubAccessToken: this.state.formValue,
+          }}
+          onSubmit={this.handleSubmit}
+          render={({ isSubmitting }) => (
+            <Form>
+              <Label htmlFor='githubAccessToken'>
+                <Title>{this.props.title}</Title>
+                {small}
+              </Label>
+              <Input id='githubAccessToken'
+                     name='githubAccessToken'
+                     disabled={isSubmitting}
+                     placeholder='personal-access-token'
+                     type='text'
+              />
+              <Submit disabled={isSubmitting} type='submit'>Submit</Submit>
+            </Form>
+          )}
+        />
+      </FormContainer>
+    </>;
   }
 }
+
+const FormContainer = styled.div`
+  margin-top: 0.5rem;
+  border-radius: 3px;
+  border: 2px solid ${theme.colors['grey-lighter']};
+  background-color: ${theme.colors['grey-lightest']};
+  padding: 0.35rem;
+`;
 
 const Label = styled.label`
   display: block;
@@ -85,65 +91,40 @@ const Small = styled.p`
   width: 50%;
   max-width: 400px;
   min-width: 300px;
-  font-size: 0.7rem;
+  font-size: 0.8rem;
   margin: 0;
   color: ${theme.colors['grey-dark']};
 `;
-// const Input = `
-//   background-color: transparent;
-//   outline: none;
-//   border: none;
-//   border-bottom: 2px solid ${theme.colors.grey};
-//   min-width: 273px;
-//   .selected & {
-//     border-bottom-color: ${theme.colors.purple};
-//   }
-// `;
-
-// const ToggleEditButton = styled.span`
-//   float: right;
-//   &:hover {
-//     cursor: pointer;
-//   }
-//   & svg {
-//     width: 2rem;
-//     height: 2rem;
-//   }
-//   & svg .primary {
-//     fill: ${theme.colors['grey-darker']};
-//   }
-//   & svg .secondary {
-//     fill: ${theme.colors.grey};
-//   }
-//   &:hover svg .primary {
-//     fill: ${theme.colors['purple-darker']};
-//   }
-//   &:hover svg .secondary {
-//     fill: ${theme.colors.purple};
-//   }
-//   &.selected svg .primary {
-//     fill: ${theme.colors['green-darker']};
-//   }
-//   &.selected svg .secondary {
-//     fill: ${theme.colors.green};
-//   }
-// `;
-
-const Submit = styled(PillButton)`
+const Input = styled.input`
   display: block;
+  font-size: 1rem;
+  background-color: transparent;
+  outline: none;
+  border: none;
+  border-radius: 3px;
+  min-width: 350px;
+  max-width: 450px;
+  width: 100%;
+  padding: 0.25rem 0.5rem 0.35rem;
   margin-top: 0.5rem;
-  background-color: ${theme.colors.grey};
-  &:hover {
-    color: ${theme.colors['']};
+  border: 1px solid ${theme.colors.purple};
+  &:disabled,
+  &[disabled] {
+    border-color: ${theme.colors.grey};
   }
 `;
 
-// const FormContainer = styled.form`
-//   margin-top: 1rem;
-//   background-color: ${theme.colors['grey-lightest']};
-//   padding: 0.5rem;
-// `;
-
-// const SettingsContainer = styled.div`
-//   padding: 0.5rem;
-// `;
+const Submit = styled.button`
+  border-radius: 999px;
+  font-size: 0.8rem;
+  font-weight: 700;
+  display: block;
+  margin-top: 0.5rem;
+  color: ${theme.colors.purple};
+  background-color: ${theme.colors.white};
+  border-color: ${theme.colors.purple};
+  &:active, &:hover, &.selected {
+    text-decoration: none;
+    outline: none;
+  }
+`;
