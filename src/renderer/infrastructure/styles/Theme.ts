402720efc6c54caaf711b1a375a8207aee2b4a61
Jason Raimondi <jason@raimondi.us>
2019-01-31 21:27:44 -0800

    Simplifying the theme, remove unused media query functions.

--- src/renderer/infrastructure/styles/Theme.ts
+++ src/renderer/infrastructure/styles/Theme.ts
@@ -1,17 +1,11 @@
-import { mediaQueries, ScreenSizesFunctions } from '@/renderer/infrastructure/styles/MediaQueries';
-
-class Theme {
-  readonly colors = require('@/data/colors.json');
-  readonly screens = require('@/data/screens.json');
-  readonly mediaQuery: ScreenSizesFunctions = mediaQueries(this.screens);
-  readonly sizes = {
+export const themeConfig = {
+  colors: require('@/data/colors.json'),
+  sizes: {
     sidebarWidth: 140,
     topbarHeight: 26,
     topnavHeight: 46,
     flashMessageHeight: 22 * 2,
     bottomNavHeight: 40,
     appDragHeight: 26,
-  };
-}
-
-export const themeConfig = new Theme();
+  },
+};
